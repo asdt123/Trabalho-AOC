1bce06e27db609394dff89a976d8a35f535d7846
asdt123 <asdtufmg@gmail.com>
2025-10-07 17:52:39 -0300

    Organização do projeto

--- run_sim.sh
+++ run_sim.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
-# Script para compilar e simular MyFAdd do Flopoco
+# Script para compilar e simular MyFAdd do Flopoco (GHDL 3.0)
+
+# Diretórios
+ENTIDADES="./entidades"
+TESTBENCH="./testbench"
+BUILD="./build"
+SAIDAS="./saidas"
+SIMULACOES="./simulações"
+
+# Cria pastas se não existirem
+mkdir -p $BUILD $SAIDAS $SIMULACOES
 
 # 1. Analisa VHDL do Flopoco
-ghdl -a -fsynopsys -fexplicit myfadd.vhd || exit 1
+ghdl -a -fsynopsys -fexplicit --workdir=$BUILD $ENTIDADES/myfadd.vhd || exit 1
 
 # 2. Analisa Testbench
-ghdl -a -fsynopsys -fexplicit tb_myfadd.vhd || exit 1
+ghdl -a -fsynopsys -fexplicit --workdir=$BUILD $TESTBENCH/tb_myfadd.vhd || exit 1
 
 # 3. Elabora o testbench
-ghdl -e -fsynopsys -fexplicit tb_MyFAdd || exit 1
+ghdl -e -fsynopsys -fexplicit --workdir=$BUILD -o $SAIDAS/tb_myfadd tb_myfadd || exit 1
 
 # 4. Roda a simulação
-ghdl -r tb_MyFAdd --vcd=MyFAdd.vcd --stop-time=500ns || exit 1
+$SAIDAS/tb_myfadd --vcd=$SIMULACOES/MyFAdd.vcd --stop-time=500ns || exit 1
 
 # 5. Abre no GTKWave
-gtkwave MyFAdd.vcd
+gtkwave $SIMULACOES/MyFAdd.vcd
